30a8d4deeb5d610fef341475096f4dc55f592b93
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-16 18:21:58 +0330

    added TaskListView

--- task/views.py
+++ task/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class TaskListView(LoginRequiredMixin,generic.ListView):
+    queryset = Task.objects.filter(is_done=False)
+    template_name = 'task/task_list.html'
+    context_object_name = 'tasks'
