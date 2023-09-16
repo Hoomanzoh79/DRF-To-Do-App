3c5a7b67c67154fe3857be700396c9ac6f6c8394
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-16 18:06:03 +0330

    added Task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    author = models.ForeignKey('accounts.Profile',on_delete = models.CASCADE)
+    title = models.CharField(max_length=250)
+    is_done = models.BooleanField(default=False)
+    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
