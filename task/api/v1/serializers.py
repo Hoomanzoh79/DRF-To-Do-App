f79089e65f471df23cf804233f8bd98ebc9c4a2f
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-19 20:13:21 +0330

    add absolute_url to serializer

--- task/api/v1/serializers.py
+++ task/api/v1/serializers.py
@@ -4,9 +4,10 @@ from accounts.models import Profile
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField(method_name='get_absolute_url')
     class Meta:
         model = Task
-        fields = ['id','title','is_done','author','datetime_created']
+        fields = ['id','title','is_done','author','datetime_created','absolute_url']
         read_only_fields = ['author']
     
 
@@ -14,3 +15,7 @@ class TaskSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         validated_data['author'] = Profile.objects.get(user_id = request.user.id)
         return super().create(validated_data)
+    
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
