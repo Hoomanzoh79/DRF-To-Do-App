308305818541a179a1769984f777196b90818898
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-19 20:00:18 +0330

    add DefaultPagination

--- task/api/v1/views.py
+++ task/api/v1/views.py
@@ -7,8 +7,18 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
+from .permissions import IsOwnerPermission
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
+from .paginations import DefaultPagination
+
 
 class TaskViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,IsOwnerPermission]
     serializer_class = TaskSerializer
     queryset = Task.objects.filter(is_done=False)
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_fields = {'author':['exact','in']}
+    search_fields = ['title']
+    ordering_fields = ['published_date']
+    pagination_class = DefaultPagination
