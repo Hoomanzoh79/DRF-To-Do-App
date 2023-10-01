6d00d7a62876f2750746d0574ae83aec77e5af77
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-10-01 11:57:38 +0330

    add api_client fixture

--- task/tests/test_task_api.py
+++ task/tests/test_task_api.py
@@ -2,11 +2,14 @@ import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
 
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    return client
 
 @pytest.mark.django_db
 class TestTaskApi:
-    def test_get_task_response_status_200(self):
-        client = APIClient()
+    def test_get_task_response_status_200(self,api_client):
         url = reverse('task:api-v1:task-list')
-        response = client.get(url)
+        response = api_client.get(url)
         assert response.status_code == 200
