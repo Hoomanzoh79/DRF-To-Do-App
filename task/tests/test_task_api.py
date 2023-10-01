8692fadca59e78830038e6ee30956632c4ee2e0c
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-10-01 16:23:11 +0330

    add new test

--- task/tests/test_task_api.py
+++ task/tests/test_task_api.py
@@ -10,7 +10,7 @@ def api_client():
 
 @pytest.fixture
 def common_user():
-    user = User.objects.create_user(email='test@test.com',Password='Aa@1234%')
+    user = User.objects.create_user(email='test@test.com',password='Aa@1234%')
     return user
 
 @pytest.mark.django_db
@@ -28,3 +28,14 @@ class TestTaskApi:
         }
         response = api_client.post(url,data)
         assert response.status_code == 401
+    
+    def test_post_task_response_status_200(self,api_client,common_user):
+        url = reverse('task:api-v1:task-list')
+        data = {
+            'title':'Go to the gym',
+            'is_done':False,
+        }
+        user = common_user
+        api_client.force_authenticate(user=user)
+        response = api_client.post(url,data)
+        assert response.status_code == 201
